e85b93ccb2f2b70bb94a48858328fe219e5dc3a1
Anthony Linsday <anthony@linsday.net>
2024-08-07 12:45:43 -0400

    initial import from old site

---
+++ index.php
@@ -0,0 +1,452 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+
+	<title>Anthony Linsday | Linux Systems Administrator</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="description" content="Anthony Linsday is an experienced Linux Systems Administrator and PHP and MySQL Developer" />
+<meta name="keywords" content="mirapoint, linux admin, systems administrator, anthony, anthony linsday, linsday, anthony ryan linsday, anthonyl, anthony l, mysql, server, linux, wilmington, servers, years, systems, message, administrator, automated, designed, website, consulting, applications, developed, installed, implemented, programming, branch, internal, house, castle, administer, backup, zendserver, kickstart, subversion, linsday, software, network, desktop, installations, apache, consolidation, bamboo, products, computer, fisheye, crucible, control, atlassian, confluence, balancers, anthony, upgrades, patches, migration, tomcat, administered, changes, bigip, tealeaf, consolidation, castle branch, branch inc, wilmington, migration, patches migration, load balancers, bigip load, linux administrator, bmc control, and implemented, upgrades patches, server upgrades, and mysql, anthony linsday, upgrades patches migration, server upgrades patches, bigip load balancers" />
+<meta name="author" content="Anthony Linsday" />
+
+	<link rel="stylesheet" type="text/css" href="./reset-fonts-grids.css?<?=filemtime('reset-fonts-grids.css')?>" media="all" /> 
+	<link rel="stylesheet" type="text/css" href="./resume.css?<?=filemtime('resume.css')?>" media="all" />
+	<link rel="stylesheet" type="text/css" href="./nav.css?<?=filemtime('nav.css')?>" media="all" />
+
+</head>
+<body>
+
+<div id="doc2" class="yui-t7">
+
+
+	<div id="inner">
+	
+		<div id="hd">
+			<div class="yui-gc">
+				<div class="yui-u first">
+					<h1>Anthony Linsday</h1>
+					<h2>Cloud Automation Engineer</h2>
+				</div>
+
+				<div class="yui-u">
+					<div class="contact-info">
+						<h3><a id="pdf" href="/resume-2023.pdf">Download Resume</a></h3>
+						<h3><a href="mailto:anthony@linsday.net">anthony@linsday.net</a></h3>
+						<h3><a href="tel:7044665581">(704) 466-5581</a></h3>
+					</div><!--// .contact-info -->
+				</div>
+			</div><!--// .yui-gc -->
+
+			<div id="nav">
+				<div class="menu-primary-container">
+				<ul id="menu-primary" class="menu">
+					<li class="current-menu-item"><a href="/" class="menu-item">My Resume</a></li>
+<!--
+					<li><a href="/projects/" class="menu-item">My Projects</a></li>
+					<li><a href="/portfolio/" class="menu-item">Portfolio</a></li>
+-->
+				</ul>
+				</div>
+			</div>
+
+		</div><!--// hd -->
+
+
+
+
+		<div id="bd">
+			<div id="yui-main">
+				<div class="yui-b">
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Summary</h2>
+						</div>
+						<div class="yui-u">
+							<p class="enlarge">
+							Long-term AWS Certified Solutions Architect with most recent years being applied to starting my own company, utilizing experience gained from previous employers.
+Over 15+ total years experience in the information technology field, focusing on infrastructure design, data center migrations and consolidation, virtualization and rapid server deployments.  
+Experienced in front-end and back-end web, api and data storage and administration.
+Both management experience, and technical expertise has always proved beneficial when working and communicating between departments, management and clients.
+							</p>
+						</div>
+					</div><!--// .yui-gf -->
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Technical Skills</h2>
+						</div>
+						<div class="yui-u">
+
+
+								<div class="talent">
+									<h2>Certifications</h2>
+									<p><a href="/linuxplus_2008.pdf">CompTIA Linux+ 2008</a></p>
+									<p><a href="/linuxplus_2009.pdf">CompTIA Linux+ 2009</a></p>
+									<p><a href="#">AWS Solutions Architect - Associate</a></p>
+								</div>
+								<div class="talent">
+									<h2>AWS Cloud</h2>
+									<p>VPC, Lambda, CodePipeline, CloudFormation, EC2, RDS, DMS, IAM, S3, AMI, EFS, EBS, Config, SNS, SQS, SSM, CloudWatch, Route53, Alexa</p>
+								</div>
+								<div class="talent">
+									<h2>Programming</h2>
+									<p>PHP, Python, REST, Native Android (Java), CodeIgniter, SAML, WordPress, Website Baker, MySQL, Perl, Bash, C, JavaScript, HTML, JAVA, Ajax, SOAP</p>
+								</div>
+
+								<div class="talent">
+									<h2>Software</h2>
+									<p>Git, SamHain, CVS, Subversion, VMWare, RedHat, Zmanda Backup, ZendServer, PXE, Hylafax,Samba, DNS, DHCP, Dovecot, LDAP, CUPS, Apache, Tomcat, MySQL, NFS, Squid proxy, Kickstart, Monetra, DRBD</p>
+								</div>
+
+								<div class="talent">
+									<h2>Hardware</h2>
+									<p>A10 load balancers, F5 BigIP load balancers, Spectra Logic, Sonicwall, Avaya PBX, VoIP, SCSI, NAS, SAN, Data and Voice Cabling</p>
+								</div>
+
+								<div class="talent">
+									<h2>Products</h2>
+									<p>RedHat Satellite, Barracuda Spam Appliances, BMC Remedy, Tealeaf, BMC Control-M, Atlassian Products [JIRA, Confluence, Crucible, Fisheye, Bamboo, Crowd], Mirapoint, RedCondor, EdgeWave</p>
+								</div>
+						</div>
+					</div><!--// .yui-gf -->
+<!--
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Technical</h2>
+						</div>
+						<div class="yui-u">
+							<ul class="talent">
+								<li>XHTML</li>
+								<li>CSS</li>
+								<li class="last">Javascript</li>
+							</ul>
+
+							<ul class="talent">
+								<li>Jquery</li>
+								<li>PHP</li>
+								<li class="last">CVS / Subversion</li>
+							</ul>
+
+							<ul class="talent">
+								<li>OS X</li>
+								<li>Windows XP/Vista</li>
+								<li class="last">Linux</li>
+							</ul>
+						</div>
+					</div> -->
+					<!--// .yui-gf-->
+
+					<div class="yui-gf">
+	
+						<div class="yui-u first">
+							<h2>Experience</h2>
+						</div><!--// .yui-u -->
+
+						<div class="yui-u">
+						
+						
+						<div class="job">
+								<h2>Carolina Brew Supply - <a href="https://www.carolinabrewsupply.com">www.carolinabrewsupply.com</a></h2>
+								<h3>Founder / Owner / Manager</h3>
+								<h4>Apr 2021 - May 2023</h4>
+<p><ul>
+<li>Designed and hosted POS software, integrated with e-commerce site.</li>
+<li>Integrated credit card processing with POS and e-commerce from Triton Processing.</li>
+<li>Integrated loyalty, marketing and rewards system from RewardPads.</li>
+<li>Designed fault-tolerant and self-healing intrastructure using AWS CloudFormation</li>
+<li>Designed and implemented AWS SQS for internal printer operations between POS system and kitchen ticket printers</li>
+<li>Used AWS SQS queues for automated print operations of shipping labels and pick tickets for e-commerce orders.</li>
+<li>Integrated DoorDash and Google Maps to custom e-commerce delivery module to estimate delivery prices and times for same-day delivery option</li>
+<li>Utilized CloudFormation, SNS, SQS, EC2, Route53, EBS, EFS</li>
+</ul>
+</p>
+</div>
+						
+						
+						
+						<div class="job">
+								<h2>EverOps / Zendesk - <a href="https://www.everops.com">www.everops.com</a></h2>
+								<h3>Contractor: CloudOps Engineer</h3>
+								<h4>Mar 2020 - Feb 2021</h4>
+<p><ul>
+<li>Used Jenkins, Kubernetes and Ansible for large scale deployments of software and infrastructure updates.</li>
+<li>Worked with 100% remote team across multiple timezones</li>
+</ul>
+</p>
+</div>
+						
+						
+						
+						
+						<div class="job">
+								<h2>Levvel - <a href="https://www.levvel.io">www.levvel.io</a></h2>
+								<h3>Contractor: Senior Automation Engineer / Cloud Architect</h3>
+								<h4>Jun 2019 - Mar 2020</h4>
+<p><ul>
+<li>Worked with a government defense contractor to automate deployments of systems and software for U.S Navy battleships
+to meet their goal of becoming "war-ready" in less than 24 hours.</li>
+<li>Used multiple AWS GovCloud accounts, VPCs and networks to emulate government SIPR and NIPR networks.</li>
+<li>Automated builds of cloud infrastructure and installation of commercial software through Ansible and Jenkins pipelines.</li>
+<li>Used JIRA, Jenkins, Ansible Tower, Nexus repository, OpenShift and AWS software and servers.</li>
+</ul>
+</p>
+</div>
+						
+						
+						
+						
+						
+<div class="job">
+								<h2>Citco - <a href="https://www.citco.com">www.citco.com</a></h2>
+								<h3>Senior Automation Engineer / Cloud Architect</h3>
+								<h4>Jan 2017 - May 2019</h4>
+<p><ul>
+<li>Automated builds of cloud infrastructure and installation of commercial software through CloudFormation and CodePipeline.</li>
+<li>Used CodePipeline to automate deployments with approvals of multiple environments from development to production</li>
+<li>Created and used CodePipeline and custom IAM roles for cross-account deployments</li>
+<li>Automated VPC creation and vpn connections for a transit VPC with hub/spoke network model</li>
+<li>Automated Setup of CloudWatch metrics and alarms to trigger SNS topics and lambda functions for integration with onpremise monitoring/alerting software</li>
+<li>Designed CloudFormation templates with custom parameters and conditions that allow for cross-region fail-over and recovery</li>
+<li>Built a server provisioning portal with php/mysql to allow teams to build servers in VMWare</li>
+<li>
+	<ul>
+	<li>Portal integrated with 3rd party apps, including: Ansible, VMWare, AWS, IPControl, Spectrum, ITAM, Active Directory</li>
+	<li>Portal also provided a REST API for callback features in Ansible</li>
+	</ul>
+</li>
+<li>Migrated several on-premise applications to AWS to help facilitate a datacenter shutdown</li>
+<li>Created Ansible playbooks to automate server configuration changes and enforcement</li>
+<li>Team lead for a group of 3 remote engineers</li>
+
+</ul></p>								
+								
+								
+								
+							</div>
+<div class="job">
+								<h2>Triton Processing</h2>
+								<h3>Head Software Engineer / Partner</h3>
+								<h4>Jul 2013 - Nov 2015</h4>
+							</div>
+<div class="job">
+								<h2>RewardPads - <a href="http://www.rewardpads.com">www.rewardpads.com</a></h2>
+								<h3>Head Software Engineer / Founder</h3>
+								<h4>Aug 2012 - Mar 2019</h4>
+							</div>
+<div class="job">
+								<h2>Shomo Live - <a href="https://www.shomolive.com">www.shomolive.com</a></h2>
+								<h3>Head Software Engineer</h3>
+								<h4>Jan 2016 - Oct 2017</h4>
+							</div>
+
+							<div class="job">
+								<h2>Cimplx ACA - <a href="https://www.cimplx.com">www.cimplx.com</a></h2>
+								<h3>Head Software Engineer / Intern Manager</h3>
+								<h4>Jul 2013 - Nov 2015</h4>
+							</div>
+
+
+
+
+
+
+
+
+							<div class="job">
+								<h2>PEQ Home Automation - <a href="https://www.mypeq.com">www.mypeq.com</a></h2>
+								<h3>Joint venture between Comporium Communications and Smart Home Ventures</h3>
+								<h4>Sept 2013 - May 2015</h4>
+<p>
+<ul>
+<li>Installed and manage infrastructure hardware for two datacenters, including:<ul>
+	<li>Cisco UCS chassis</li>
+	<li>A10 Load balancers</li>
+	<li>Palo Alto firewalls</li>
+	<li>EMC VNX SAN</li>
+	<li>Cisco Nexus 5000 switches</li>
+	<li>RecoverPoint appliances</li>
+	<li>Site Recovery Manager</li>
+	</ul>
+</li>
+<li>Managed virtual infrastructure using: <ul>
+	<li>VMWare vCenter</li>
+	<li>RedHat Satellite</li>
+	<li>SamHain file integrity monitor</li>
+	</ul>
+</li>
+<li>Planned and implemented geo-redundant datacenter backups and replication</li>
+<li>Maintain infrastructure level hardware and firmware updates</li>
+<li>Maintain operating system and application level updates</li>
+<li>Ensure maximum uptime with redundant hardware, virtual servers and load balancers</li>
+<li>Trained junior level database administrators</li>
+<li>Communicated requirements with development team to ensure their software will handle failover and redundancy</li>
+<li>Automated PHP and Java code deployments to eliminate downtime and maintenance windows</li>
+</ul>
+</p>
+							</div>
+
+
+							<div class="job">
+								<h2>Comporium Communications - Rock Hill, SC</h2>
+								<h3>IT Systems Engineer IV / Information Technology Group</h3>
+								<h4>Mar 2012 - May 2015</h4>
+<p><ul>
+<li>Designed platform for mobile controlled water fountain in <a href="http://fountainparkrockhill.com/">Fountain Park, Rock Hill</a></li>
+<li>Setup camera to capture <a href="https://www.youtube.com/watch?v=brs_Op3G2Jc">time-lapse video</a> of <a href="http://fountainparkrockhill.com/">Fountain Park</a>'s construction</li>
+<li>Programmed web interface dashboard for support center to manage over 200,000 customers</li>
+<li>Programmed file transfer system for<ul>
+	<li>sending late accounts to collection agencies</li>
+	<li>getting 911 call usage from mobile carriers</li>
+	<li>sending billing information to invoice printing company</li>
+	</ul>
+</li>
+<li>Planned and implemented centralized monitoring, alerting and logging systems</li>
+<li>Upgraded tape backup appliances and replaced server with ZManda</li>
+<li>Designed physical and logical network layout for Immedion's new datacenter</li>
+<li>Designed SAML authentication for integration with 2012 NBC Olympics</li>
+<li>Manage authoritative DNS server over 180 zones</li>
+<li>Maintain Mirapoint email platform for over 200,000 internet and email subscribers</li>
+<li>Manage web and FTP servers for internet subscribers' Personal Web Space</li>
+<li>Coordinated deployment and migration of spam filtering from RedCondor appliances to Barracuda</li>
+<li>Currently planning migration of email platform to cloud service</li>
+</ul></p>
+
+							</div>
+
+
+
+							<div class="job">
+								<h2>Wells Fargo - Charlotte, NC</h2>
+								<h3>Systems Support Engineer / International Shared Services Technology</h3>
+								<h4>Aug 2011 - Mar 2012</h4>
+<p><ul>
+<li>Manage server and software upgrades</li>
+<li>Managed architectural diagrams for six internal applications</li>
+<li>Automated scripts in PHP to insert 180+ users for new application, saving numerous work days</li>
+<li>Built and transferred internal WebSphere application to new hosting environment</li>
+<li>Concurrently built  WebSphere environment while upgrading application on existing servers</li>
+<li>Coordinated a simulated Disaster Recovery scenario for multiple production applications</li>
+<li>Decommissioned Wachovia's retired Anti Money Laundering applications  and servers </li>
+<li>Built and supported Linux, Apache, MySQL  servers for development teams</li>
+<li>Deployed automated scanners and imaging applications for multiple Lines of Business</li>
+<li>Managed change requests in a controlled environment</li>
+<li>Restored credibility of the support team for various Lines of Business</li>
+<li>Functionality and Site Integration Testing for application upgrades and deployments</li>
+</ul></p>
+
+							</div>
+
+							<div class="job">
+								<h2>Edvance360.com - Charlotte, NC</h2>
+								<h3>PHP Programmer / Web Development</h3>
+								<h4>Mar 2011 - Jul 2011</h4>
+<p><ul>
+<li>PHP development for education software</li>
+<li>MySQL database administration </li>
+<li>Built and monitored backup server</li>
+<li>Converted PHP code from procedural to object-oriented</li>
+<li>Integrated existing website with TurnItIn API plagiarism detection service via SOAP</li>
+</ul></p>
+							</div>
+
+							<div class="job">
+								<h2>American Tire Distributors, Inc. - Charlotte, NC</h2>
+								<h3>LAMP Operations Administrator / Information Technology</h3>
+								<h4>Jan 2009 - Feb 2011</h4>
+<p><ul>
+<li>Architect/Engineer for server farms for websites such as ATDOnline.com and TireBuyer.com</li>
+<li>Installed/configured F5 load balancers for high traffic websites, pricing and inventory queries</li>
+<li>Implemented Nagios and Cacti server monitoring</li>
+<li>Administered Apache, ZendServer, Tomcat and MySQL servers</li>
+<li>Automated PHP and Java code deployments</li>
+<li>Server upgrades, patches, migration and consolidation</li>
+<li>Built and administered Atlassian servers [JIRA, FishEye, Crucible, Confluence and Bamboo]</li>
+<li>Designed and implemented web and MySQL failover, backup and DR solutions</li>
+<li>Installed Tealeaf web capture and replay appliance</li>
+<li>Installed and administered BMC Control-M job scheduler</li>
+</ul></p>
+							</div>
+
+							<div class="job">
+								<h2>Castle Branch, Inc. - Wilmington, NC</h2>
+								<h3>Linux Systems Administrator / Information Technology</h3>
+								<h4>Jul 2006 - Dec 2008</h4>
+<p><ul>
+<li>Architected and implemented infrastructure for user and customer facing servers</li>
+<li>Maintained PCI compliance for credit card processors</li>
+<li>Built predictive dialer server based on Asterisk and ViciDial</li>
+<li>Created standards for server builds and documentation</li>
+<li>Oversaw integration of in-house developed applications</li>
+<li>Coordinated server upgrades, patches, migrations and consolidations</li>
+<li>Server baselines, backups, backup integrity testing, and restorations</li>
+<li>Automated desktop/server installations via PXE network book with dynamic PHP kickstart files</li>
+<li>Specialized in interoperability of desktop and server operating systems and software		</li>
+<li>Developed and debugged in-house Perl applications</li>
+<li>Converted multiple websites from Perl to object oriented PHP</li>
+<li>Developed internal PHP application with LDAP integration for tracking personnel changes</li>
+</ul></p>
+							</div>
+
+							<div class="job last">
+								<h2>New Hanover County Schools - Wilmington, NC</h2>
+								<h3>IT Apprentice / Information Technology</h3>
+								<h4>Aug 2004 - Jun 2006</h4>
+<p><ul>
+<li>Deployed over 400 new PC throughout Hoggard High School</li>
+<li>Used Novell ZenWorks to automate software deployments all desktop computers</li>
+<li>Everyday support for PCs, printers, scanners and network components</li>
+<li>Maintained Hoggard High School.s website content</li>
+</ul></p>
+							</div>
+
+						</div><!--// .yui-u -->
+					</div><!--// .yui-gf -->
+
+
+					<div class="yui-gf last">
+						<div class="yui-u first">
+							<h2>Education</h2>
+						</div>
+						<div class="yui-u">
+							<div class="job last">
+								<h2>Cape Fear Community College - Wilmington, NC</h2>
+								<h3>Computer Information Systems</h3>
+								<h3>Computer Engineering Technology</h3>
+								<h4>4.0 GPA</h4>
+							</div>
+						</div>
+					</div><!--// .yui-gf -->
+
+
+				</div><!--// .yui-b -->
+			</div><!--// yui-main -->
+		</div><!--// bd -->
+
+		<div id="ft">
+			<p>Anthony Linsday &mdash; <a href="mailto:anthony@linsday.net">anthony@linsday.net</a> &mdash; (704) 665-0183</p>
+		</div><!--// footer -->
+
+	</div><!-- // inner -->
+
+
+</div><!--// doc -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-21043147-2']);
+  _gaq.push(['_setDomainName', '.anthonyl.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
+</body>
+</html>
+
